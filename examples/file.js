5e1308f0a265dbb28fb210584a2900c7b9910e63
Jascha Ehrenreich <jascha@jaeh.at>
2018-05-22 18:10:08 +0200

    remove examples dir

--- examples/file.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-const fs = require('fs');
-const zopfli = require('node-zopfli-es');
-
-fs.createReadStream('file.js')
-  .pipe(new zopfli.createGzip())
-  .pipe(fs.createWriteStream('file.js.gz'));
